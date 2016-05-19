e217cf5352952864ae81d1feba56d0e76326c118
Meghal Donde <meghal.donde@gmail.com>
2016-05-19 16:21:47 -0400

    categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,22 @@
 class CategoriesController < ApplicationController
-	def category_params
+  
+
+  # GET /categories
+  # GET /categories.json
+  def index
+    @categories = Category.all
+  end
+
+  # GET /categories/1
+  # GET /categories/1.json
+  def show
+	@category = Category.find(params[:id])
+	@title = @category.name
+	@posts = @category.posts
+  end
+
+ # Never trust parameters from the scary internet, only allow the white list through.
+    def category_params
       params.require(:category).permit(:name)
     end
-	
-  def index
-  end
-
-  def edit
-  end
-
-  def show
-  end
-
-  def new
-  end
 end
